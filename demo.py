9b9624d8e28de5dedea91eef12a0fbe402d53494
mohitdhumal2005 <mohitdhumal2005@gmail.com>
2026-09-10 15:03:59 +0530

    https://github.com/mohitdhumal2005/ImccBridge10.git

---
+++ demo.py
@@ -0,0 +1,6 @@
+a= input("Enter a no")
+b=5
+c =a+b
+print(c)
+print(a/b)
+
---
+++ evenOdd.py
@@ -0,0 +1,5 @@
+#a = int(input("Enter Number:"))
+#print("Number is odd : ", a%2!=0)
+
+age=int(input("Enter age:"))
+print(age,"years = ", age*365,"days")
